9a4eac231226bd2d381a7b3ce8c0217f7c8d0134
davidmartinezbosch45 <david.martinez.bosch45@gmail.com>
2022-05-25 23:49:33 +0200

    hola3nuevo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.david.mavenproject4 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.david.mavenproject4 to javafx.fxml;
+    exports com.david.mavenproject4;
+}
+*/
